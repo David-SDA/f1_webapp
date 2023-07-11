de32d07569e81818f41915c409ac87499230cfcc
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-11 12:49:31 +0200

    Ajout partiel de la barre de navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,32 @@
 import '../styles/navBar.css';
 import '../fonts/Formula1-Bold.ttf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar(){
     return (
-        <nav className="navBar">
-            <h1>Title</h1>
-            <ul>
-                <li><a href="#">Schedule</a></li>
-                <li><a href="#">Standings</a></li>
-                <li><a href="#">History</a></li>
-            </ul>
-        </nav>
+        <header className="navBar">
+            <h1><a href='#' className='title'>Title</a></h1>
+            <div className='navList'>
+                <div>
+                    <a href='#' className='navLink'>Schedule</a>
+                </div>
+                <div className='subNav'>
+                    <button className='subNavButton'>Standings <FontAwesomeIcon icon={faCaretDown} /></button>
+                    <div className='subNavContent'>
+                        <a href="#">Drivers</a>
+                        <a href="#">Constructors</a>
+                    </div>
+                </div>
+                <div className='subNav'>
+                    <button className='subNavButton'>History <FontAwesomeIcon icon={faCaretDown} /><i class="fa-solid fa-caret-down"></i></button>
+                    <div className='subNavContent'>
+                        <a href="#">Drivers Champions</a>
+                        <a href="#">Constructors Champions</a>
+                        <a href="#">Circuits</a>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
