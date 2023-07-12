bfee5879b02f0f258e23f62e9880e82de77df445
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-12 16:50:58 +0200

    Ajout d'un composant pour le conteneur de course

--- src/components/NextRace.js
+++ src/components/NextRace.js
@@ -1,68 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import '../styles/NextRace.css';
-
-import ScheduleNormalWeekend from "./raceComponents/ScheduleNormalWeekend";
-import ScheduleSprintWeekend from "./raceComponents/ScheduleSprintWeekend";
-import RaceHeader from "./raceComponents/RaceHeader";
+import RaceContainer from "./raceComponents/RaceContainer";
 
 export default function NextRace(){
-    const [race, setRace] = useState([]);
-
-    const fetchInfo = async () => {
-        try{
-            const response = await fetch('https://ergast.com/api/f1/current/next.json');
-            const data = await response.json();
-            setRace(data.MRData.RaceTable.Races[0]);
-            console.log(race);
-        }catch(error){
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchInfo();
-    }, []);
-
-    let dateFP1 = new Date(race?.FirstPractice?.date + 'T' + race?.FirstPractice?.time);
-    let dateFP2 = new Date(race?.SecondPractice?.date + 'T' + race?.SecondPractice?.time);
-    let dateQuali = new Date(race?.Qualifying?.date + 'T' + race?.Qualifying?.time);
-    let dateRace = new Date(race?.date + 'T' + race?.time);
-
-    if(race?.ThirdPractice){
-        var dateFP3 = new Date(race?.ThirdPractice?.date + 'T' + race?.ThirdPractice?.time);
-    }
-    else{
-        var dateSprint = new Date(race?.Sprint?.date + 'T' + race?.Sprint?.time);
-    }
-
     return (
         <div className="nextRaceContainer">
             <h1 className="titleNextRace">Next Race</h1>
-            <div className="raceContainer">
-                <RaceHeader
-                    round={race?.round}
-                    raceName={race?.raceName}
-                    country={race?.Circuit?.Location?.country}
-                />
-                {
-                    (dateFP3) ? 
-                        <ScheduleNormalWeekend
-                            dateFP1={dateFP1}
-                            dateFP2={dateFP2}
-                            dateFP3={dateFP3}
-                            dateQuali={dateQuali}
-                            dateRace={dateRace} 
-                        />
-                    :
-                        <ScheduleSprintWeekend
-                            dateFP1={dateFP1}
-                            dateQuali={dateQuali}
-                            dateFP2={dateFP2}
-                            dateSprint={dateSprint}
-                            dateRace={dateRace}
-                        />
-                }
-            </div>
+            <RaceContainer round={'next'} />
         </div>
     );
 }
---
+++ src/components/raceComponents/RaceContainer.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import '../../styles/raceComponentsStyles/raceContainer.css';
+
+import RaceHeader from './RaceHeader';
+import ScheduleNormalWeekend from './ScheduleNormalWeekend';
+import ScheduleSprintWeekend from './ScheduleSprintWeekend';
+
+export default function RaceContainer({round}){
+    const [race, setRace] = useState([]);
+
+    const fetchInfo = async () => {
+        try{
+            const response = await fetch('https://ergast.com/api/f1/current/' + round + '.json');
+            const data = await response.json();
+            setRace(data.MRData.RaceTable.Races[0]);
+            console.log(race);
+        }catch(error){
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchInfo();
+    }, []);
+
+    let dateFP1 = new Date(race?.FirstPractice?.date + 'T' + race?.FirstPractice?.time);
+    let dateFP2 = new Date(race?.SecondPractice?.date + 'T' + race?.SecondPractice?.time);
+    let dateQuali = new Date(race?.Qualifying?.date + 'T' + race?.Qualifying?.time);
+    let dateRace = new Date(race?.date + 'T' + race?.time);
+
+    if(race?.ThirdPractice){
+        var dateFP3 = new Date(race?.ThirdPractice?.date + 'T' + race?.ThirdPractice?.time);
+    }
+    else{
+        var dateSprint = new Date(race?.Sprint?.date + 'T' + race?.Sprint?.time);
+    }
+    return (
+        <div className="raceContainer">
+            <RaceHeader
+                round={race?.round}
+                raceName={race?.raceName}
+                country={race?.Circuit?.Location?.country}
+            />
+            {
+                (dateFP3) ? 
+                    <ScheduleNormalWeekend
+                        dateFP1={dateFP1}
+                        dateFP2={dateFP2}
+                        dateFP3={dateFP3}
+                        dateQuali={dateQuali}
+                        dateRace={dateRace} 
+                    />
+                :
+                    <ScheduleSprintWeekend
+                        dateFP1={dateFP1}
+                        dateQuali={dateQuali}
+                        dateFP2={dateFP2}
+                        dateSprint={dateSprint}
+                        dateRace={dateRace}
+                    />
+            }
+        </div>
+    );
+}
