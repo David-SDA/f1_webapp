190f346a5ea2f778b71e54c19b439fcb3905cbcc
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-13 12:31:48 +0200

    Ajustements de l'alt du drapeau + ajout de package

--- src/components/raceComponents/FlagTitleRace.jsx
+++ src/components/raceComponents/FlagTitleRace.jsx
@@ -5,7 +5,7 @@ import '../../styles/raceComponentsStyles/flagTitleRace.css';
 export default function FlagTitleRace({raceName, country}){
     return (
         <>
-            <img src={flags[country]} alt="Flag image" className="countryFlag" />
+            <img src={flags[country]} alt="Country's flag of the race location" className="countryFlag" />
             <p className="raceName">{raceName}</p>
         </>
     );
