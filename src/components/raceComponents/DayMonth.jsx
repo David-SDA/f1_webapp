2b23496a75863fc4405a67fd25e9696fed912ccf
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-18 14:33:35 +0200

    Adaptation en bootstrap du composant DayMonth

--- src/components/raceComponents/DayMonth.jsx
+++ src/components/raceComponents/DayMonth.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import '../../styles/raceComponentsStyles/dayMonth.css';
+
+import { Container } from "react-bootstrap";
 
 export default function DayMonth({date}){
     return (
-        <div className="scheduleDate">
-            <p className="scheduleDateNumber">{date.toLocaleDateString('en-GB', {day: '2-digit'})}</p>
-            <p className="scheduleDateMonth">{date.toLocaleDateString('en-GB', {month: 'short'})}</p>
-        </div>
+        <Container className="d-flex flex-column align-items-center ms-2 me-2">
+            <span className="h4 mb-0" style={{fontFamily: "Formula1-Bold"}}>{date.toLocaleDateString('en-GB', {day: '2-digit'})}</span>
+            <span className="rounded-5 text-center ps-3 pe-3" style={{backgroundColor: "#e8e8e8", fontFamily: "Formula1-Regular"}}>{date.toLocaleDateString('en-GB', {month: 'short'})}</span>
+        </Container>
     );
 }
