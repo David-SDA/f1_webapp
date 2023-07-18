71c780f3b6719f4cbad63428af48ae541fb851d4
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-18 10:13:36 +0200

    Adaptation en bootstrap du composant Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import '../styles/NextRace.css';
 import RaceContainer from "./raceComponents/RaceContainer";
+
+import { Container } from "react-bootstrap";
 
 export default function Home(){
     return (
-        <div className="nextRaceContainer">
-            <h1 className="titleNextRace">Next Race</h1>
+        <Container className="d-flex flex-column justify-content-center align-items-center border border-primary">
+            <h1 className="align-self-start fst-italic ms-5" style={{fontFamily: 'Formula1-Regular'}} >Next Race</h1>
             <RaceContainer round={'next'} />
-        </div>
+        </Container>
     );
 }
