67d133ce2c1dbf674c77144eeb42813297bfd65e
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-18 14:17:02 +0200

    Adaptation en bootstrap des composants Schedule

--- src/components/raceComponents/ScheduleSprintWeekend.jsx
+++ src/components/raceComponents/ScheduleSprintWeekend.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import '../../styles/raceComponentsStyles/scheduleNormalAndSprintWeekend.css';
+
 import ScheduleOneSession from './ScheduleOneSession';
+
+import { Container } from "react-bootstrap";
 
 export default function ScheduleNormalWeekend({dateFP1, dateQuali, dateFP2, dateSprint, dateRace}){
     return (
-        <>
-            <h2 className="scheduleText">SCHEDULE</h2>
-            <div className="allSchedule">
+        <Container>
+            <h3 className="ms-3 mt-2 fst-italic" style={{fontFamily: 'Formula1-Bold'}}>SCHEDULE</h3>
+            <Container className="d-flex flex-column">
                 <ScheduleOneSession sessionName={'RACE'} date={dateRace} />
                 <ScheduleOneSession sessionName={'SPRINT'} date={dateSprint} />
                 <ScheduleOneSession sessionName={'PRACTICE 2'} date={dateFP2} />
                 <ScheduleOneSession sessionName={'QUALIFYING'} date={dateQuali} />
                 <ScheduleOneSession sessionName={'PRACTICE 1'} date={dateFP1} />
-            </div>
-        </>
+            </Container>
+        </Container>
     );
 }
