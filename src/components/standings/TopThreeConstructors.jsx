e4a21b207af67cf9522f9f0092a44ef5afb57f8a
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-19 11:44:20 +0200

    Ajout des données pour le top 3 constructeurs

--- src/components/standings/TopThreeConstructors.jsx
+++ src/components/standings/TopThreeConstructors.jsx
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import SmallConstructor from "./SmallConstructor";
+import { currentConstructorColor } from "../../constants/currentConstructorColor";
+import { currentConstructorSmallText } from "../../constants/currentConstructorSmallText";
 
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 
 export default function TopThreeConstructors(){
-    return (
-        <Container className="p-0">
-            <SmallConstructor position={"1"} name={"Alpine F1 Team"} />
-            <SmallConstructor position={"2"} name={"Aston Martin"} />
-            <SmallConstructor position={"3"} name={"Alphatauri"} />
-        </Container>
-    );
+    const [standings, setStandings] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchInfo = async () => {
+        try{
+            const response = await fetch('https://ergast.com/api/f1/current/constructorStandings.json?limit=3');
+            const data = await response.json();
+            setStandings(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
+        }catch(error){
+            console.log(error);
+        }finally{
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchInfo();
+    }, []);
+
+    if(isLoading){
+        return(
+            <Spinner animation="border" style={{color: "#ff1801"}} />
+        );
+    }
+    else{
+        return (
+            <Container className="p-0">
+                {
+                    standings.map((constructor, index) => {
+                        return (
+                            <SmallConstructor
+                                position={constructor?.position}
+                                name={currentConstructorSmallText[constructor?.Constructor?.constructorId]}
+                                points={constructor?.points}
+                                color={currentConstructorColor[constructor?.Constructor?.constructorId]}
+                                key={index}
+                            />
+                        );
+                    })
+                }
+            </Container>
+        );
+    }
 }
---
+++ src/constants/currentConstructorSmallText.jsx
@@ -0,0 +1,12 @@
+export const currentConstructorSmallText = {
+    'red_bull': 'Red Bull',
+    'ferrari': 'Ferrari',
+    'mercedes': 'Mercedes',
+    'aston_martin': 'Aston Martin',
+    'alpine': 'Alpine',
+    'mclaren': 'McLaren',
+    'williams': 'Williams',
+    'haas': 'Haas',
+    'alfa': 'Alfa Romeo',
+    'alphatauri': 'AlphaTauri',
+}
