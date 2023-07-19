c9e39ad1977fcc8b704cba1f8c4f249d1bd4581d
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-19 17:00:40 +0200

    Début du style de la page Schedule

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,19 +1,87 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Image, Row, Spinner } from "react-bootstrap";
+import { flags } from "../constants/flags";
 
 export default function Schedule() {
+    const myBorder = {
+        fontFamily: "Formula1-Regular",
+        borderBottom: "5px solid #ff1801",
+        borderRight: "5px solid #ff1801",
+        borderRadius: 10,
+        minHeight: 200,
+    };
+
+    const dateStyle = {
+        fontFamily: "Formula1-Wide",
+        fontSize: 12,
+    }
+
     const [schedule, setSchedule] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    return (
-        <Container className="border border-primary">
-            <h2 className="fst-italic" style={{fontFamily: "Formula1-Regular"}}>F1 2023 Schedule</h2>
-            <Row>
-                <Col sm={6} md={4} lg={3}>
-                    <Container className="border border-black"><p>ici</p></Container>
-                </Col>
-            </Row>
-        </Container>
-    );
+    const fetchInfo = async () => {
+        try{
+            const response = await fetch("http://ergast.com/api/f1/current.json");
+            const data = await response.json();
+            setSchedule(data.MRData.RaceTable.Races);
+        }catch(error){
+            console.log(error);
+        }finally{
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchInfo();
+    }, []);
+
+    if(isLoading){
+        return (
+            <Spinner animation="border" className="mt-2 align-self-center" style={{color: "#ff1801"}} />
+        );
+    }
+    else{
+        return (
+            <Container className="mt-2">
+                <h2 className="mb-4 p-3 fst-italic" style={{fontFamily: "Formula1-Regular"}}>F1 2023 Schedule</h2>
+                <Row className="d-flex flex-wrap">
+                    {
+                        schedule.map((race, index) => {
+                            const dateDebut = new Date(race?.FirstPractice?.date);
+                            const dateFin = new Date(race?.date);
+
+                            console.log(dateDebut);
+                            console.log(dateFin);
+
+                            return (
+                                <Col sm={6} md={4} lg={3} className="mb-3" key={index}>
+                                    <a href="#" className="link-dark link-underline-opacity-0 link-opacity-50-hover">
+                                        <Container style={myBorder} >
+                                            <Container className="d-flex justify-content-between align-items-center p-0">
+                                                <p className="m-0" style={{fontFamily: "Formula1-Regular"}}>Round {race?.round}</p>
+                                                <Image src={flags[race?.Circuit?.Location?.country]} alt={"Flag of " + race?.Circuit?.Location?.country} className="border rounded-3" height={30} />
+                                            </Container>
+                                            <Container className="d-flex flex-row align-items-center mt-2 border-2 border-black border-bottom border-end rounded-4">
+                                                <Container className="d-flex flex-column align-items-center">
+                                                    <p className="m-0" style={dateStyle}>{dateDebut.toLocaleDateString("en-GB", {day: "2-digit"})}</p>
+                                                    <p className="m-0" style={dateStyle}>{dateDebut.toLocaleDateString("en-GB", {month: "short"})}</p>
+                                                </Container>
+                                                <p style={{fontFamily: "Formula1-Wide"}}>-</p>
+                                                <Container className="d-flex flex-column align-items-center">
+                                                    <p className="m-0" style={dateStyle}>{dateFin.toLocaleDateString("en-GB", {day: "2-digit"})}</p>
+                                                    <p className="m-0" style={dateStyle}>{dateFin.toLocaleDateString("en-GB", {month: "short"})}</p>
+                                                </Container>
+                                            </Container>
+                                            <p className="text-center mt-2" style={{fontFamily: "Formula1-Bold", letterSpacing: "0.0001rem"}}>{race?.raceName} &gt;</p>
+                                        </Container>
+                                    </a>
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
+            </Container>
+        );
+    }
 }
