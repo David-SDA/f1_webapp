ae836b3d815bf7ab82a6fe9d034e4b7eefe6e921
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-19 12:15:26 +0200

    Ajout de lien dans le top 3 pour plus tard

--- src/components/standings/SmallConstructor.jsx
+++ src/components/standings/SmallConstructor.jsx
@@ -10,11 +10,13 @@ export default function SmallConstructor({position, name, points, color}){
     };
 
     return (
-        <Container className="d-flex flex-row justify-content-start align-items-center bg-white mb-2 rounded-3 p-2">
-            <span style={{fontFamily: "Formula1-Wide", width: 23}}>{position}</span>
-            <div className="ms-1 rounded-3" style={MyBar}></div>
-            <span className="h5 ms-1 mb-0" style={{fontFamily: "Formula1-Bold", letterSpacing: "0.001rem"}}>{name}</span>
-            <span className="text-center ms-auto p-2 rounded-5" style={{fontFamily: "Formula1-Bold", backgroundColor: "#e8e8e8", fontSize: 12}}>{points} <span style={{fontFamily: "Formula1-regular"}}>PTS</span></span>
-        </Container>
+        <a href="#" className="link-dark link-underline-opacity-0 link-opacity-75-hover">
+            <Container className="d-flex flex-row justify-content-start align-items-center bg-white mb-2 rounded-3 p-2">
+                <span style={{fontFamily: "Formula1-Wide", width: 23}}>{position}</span>
+                <div className="ms-1 rounded-3" style={MyBar}></div>
+                <span className="h5 ms-1 mb-0" style={{fontFamily: "Formula1-Bold", letterSpacing: "0.001rem"}}>{name}</span>
+                <span className="text-center ms-auto p-2 rounded-5" style={{fontFamily: "Formula1-Bold", backgroundColor: "#e8e8e8", fontSize: 12}}>{points} <span style={{fontFamily: "Formula1-regular"}}>PTS</span></span>
+            </Container>
+        </a>
     );
 }
