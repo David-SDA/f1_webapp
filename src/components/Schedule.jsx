439859b579151edb2497ae55b7a6542ff9249ae8
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-19 12:58:16 +0200

    Début de la construction de la page Schedule

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Schedule() {
+    const [schedule, setSchedule] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     return (
-        <div>
-            <p>Schedule</p>
-        </div>
+        <Container className="border border-primary">
+            <h2 className="fst-italic" style={{fontFamily: "Formula1-Regular"}}>F1 2023 Schedule</h2>
+            <Row>
+                <Col sm={6} md={4} lg={3}>
+                    <Container className="border border-black"><p>ici</p></Container>
+                </Col>
+            </Row>
+        </Container>
     );
 }
