b572984d9ae2ebe26faee8cd074e762c989080e8
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-21 16:26:55 +0200

    Ajout d'un composant RaceResultsContentContainer

--- src/components/raceResults/RaceResultsContainer.jsx
+++ src/components/raceResults/RaceResultsContainer.jsx
@@ -2,41 +2,26 @@ import React from "react";
 
 import RaceResultsHeaderContainer from "./RaceResultsHeaderContainer";
 
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import RaceResultsContentContainer from "./RaceResultsContentContainer";
 
 export default function RaceResultsContainer(){
-    const MyBar = {
-        width: 7,
-        height: 25,
-        backgroundColor: "red",
-    };
-
     return (
         <Container className="p-0">
             <p className="h1 align-self-start fst-italic" style={{fontFamily: "Formula1-Regular", letterSpacing: "0.0005rem"}}>Race Results</p>
             <Container className="d-flex flex-column p-0 p-sm-2 rounded" style={{backgroundColor: "#38383f"}}>
                 <RaceResultsHeaderContainer />
-                <Row className="d-flex flex-nowrap flex-row justify-content-around align-items-center bg-white m-1 p-1 p-sm-2 rounded-3">
-                    <Col className="p-0" xs={1} sm={1} md={1} lg={1}>
-                        <p className="m-0 text-center text-xs" style={{fontFamily: "Formula1-Wide", letterSpacing: "0.0005rem"}}>1</p>
-                    </Col>
-                    <Col className="d-flex flex-row justify-content-center align-items-center" xs={2} sm={1} md={1} lg={1}>
-                        <div className="rounded-3" style={MyBar}></div>
-                    </Col>
-                    <Col className="p-0" xs={2} sm={6} md={4} lg={5}>
-                        <p className="m-0 d-none d-sm-block" style={{fontFamily: "Formula1-Bold", letterSpacing: "0.0005rem"}}><span style={{fontFamily: "Formula1-Regular", letterSpacing: "0.0005rem"}}>Max</span> Verstappen</p>
-                        <p className="m-0 d-block d-sm-none" style={{fontFamily: "Formula1-Bold", letterSpacing: "0.0005rem"}}>VER</p>
-                    </Col>
-                    <Col className="p-0 d-none d-md-block" md={3} lg={2}>
-                        <p className="m-0 text-center" style={{fontFamily: "Formula1-Regular", letterSpacing: "0.0005rem"}}><span className="rounded-5 p-1 text-white" style={{backgroundColor: "purple"}}>1:30:275</span></p>
-                    </Col>
-                    <Col className="p-0"  xs={5} sm={3} md={2} lg={2}>
-                        <p className="m-0 text-center" style={{fontFamily: "Formula1-Regular", letterSpacing: "0.0005rem"}}><span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>1:25:16:938</span></p>
-                    </Col>
-                    <Col className="p-0" xs={2} sm={1} md={1} lg={1}>
-                        <p className="m-0 text-center" style={{fontFamily: "Formula1-Black", letterSpacing: "0.0005rem"}}>26</p>
-                    </Col>
-                </Row>
+                <RaceResultsContentContainer
+                    position={"2"}
+                    color={"red"}
+                    firstName={"Charles"}
+                    familyName={"Leclerc"}
+                    code={"LEC"}
+                    fastestLap={"1:28:358"}
+                    fastestLapRank={"1"}
+                    totalTime={"1:23:35:247"}
+                    points={"18"}
+                />
             </Container>
         </Container>
     );
---
+++ src/components/raceResults/RaceResultsContentContainer.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+import { Col, Row } from "react-bootstrap";
+
+export default function RaceResultsContentContainer({
+    position,
+    color,
+    firstName,
+    familyName,
+    code, 
+    fastestLap,
+    fastestLapRank,
+    totalTime,
+    points
+}){
+    const MyBar = {
+        width: 7,
+        height: 25,
+        backgroundColor: color,
+    };
+
+    const regularText = {
+        fontFamily: "Formula1-Regular",
+        letterSpacing: "0.0005rem",
+    };
+
+    const boldText = {
+        fontFamily: "Formula1-Bold",
+        letterSpacing: "0.0005rem",
+    };
+
+    const blackText = {
+        fontFamily: "Formula1-Black",
+        letterSpacing: "0.0005rem",
+    };
+
+    const wideText = {
+        fontFamily: "Formula1-Wide",
+        letterSpacing: "0.0005rem",
+    }
+
+    return (
+        <Row className="d-flex flex-nowrap flex-row justify-content-around align-items-center bg-white m-1 p-1 p-sm-2 rounded-3">
+            <Col className="p-0" xs={1} sm={1} md={1} lg={1}>
+                <p className="m-0 text-center text-xs" style={wideText}>
+                    {position}
+                </p>
+            </Col>
+            <Col className="d-flex flex-row justify-content-center align-items-center" xs={2} sm={1} md={1} lg={1}>
+                <div className="rounded-3" style={MyBar}></div>
+            </Col>
+            <Col className="p-0" xs={2} sm={6} md={4} lg={5}>
+                <p className="m-0 d-none d-sm-block" style={boldText}>
+                    <span style={regularText}>{firstName} </span> 
+                    {familyName}
+                </p>
+                <p className="m-0 d-block d-sm-none" style={boldText}>
+                    {code}
+                </p>
+            </Col>
+            <Col className="p-0 d-none d-md-block" md={3} lg={2}>
+                <p className="m-0 text-center" style={regularText}>
+                    {
+                        fastestLapRank === "1" ? (
+                            <span className="rounded-5 p-1 text-white" style={{backgroundColor: "purple"}}>{fastestLap}</span>
+                        ) : (
+                            <span className="rounded-5 p-1">{fastestLap}</span>
+                        )
+                    }
+                </p>
+            </Col>
+            <Col className="p-0"  xs={5} sm={3} md={2} lg={2}>
+                <p className="m-0 text-center" style={regularText}>
+                    <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{totalTime}</span>
+                </p>
+            </Col>
+            <Col className="p-0" xs={2} sm={1} md={1} lg={1}>
+                <p className="m-0 text-center" style={blackText}>
+                    {points}
+                </p>
+            </Col>
+        </Row>
+    );
+}
