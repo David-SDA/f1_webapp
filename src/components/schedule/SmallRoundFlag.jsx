46ae5b0aafdc64a4860a14017d6d797d29e68c27
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-21 10:01:54 +0200

    Création d'un composant SmallRoundFlag

---
+++ src/components/schedule/SmallRoundFlag.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { flags } from "../../constants/flags";
+
+import { Container, Image } from "react-bootstrap";
+
+export default function SmallRoundFlag({round, country}){
+    return (
+        <Container className="d-flex justify-content-between align-items-center p-0">
+            <p className="m-0" style={{fontFamily: "Formula1-Regular"}}>Round {round}</p>
+            <Image src={flags[country]} alt={"Flag of " + country} className="border rounded-3" height={30} />
+        </Container>
+    );
+}
