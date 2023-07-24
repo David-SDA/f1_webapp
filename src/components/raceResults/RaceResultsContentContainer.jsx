b6c3deed5fa000851c83b7dffb71ba74684bca48
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-24 09:48:47 +0200

    Ajout d'un lien pour les pilotes

--- src/components/raceResults/RaceResultsContentContainer.jsx
+++ src/components/raceResults/RaceResultsContentContainer.jsx
@@ -41,54 +41,56 @@ export default function RaceResultsContentContainer({
     }
 
     return (
-        <Row className="d-flex flex-nowrap flex-row justify-content-around align-items-center bg-white m-1 p-1 p-sm-2 rounded-3">
-            <Col className="p-0" xs={1} sm={1} md={1} lg={1}>
-                <p className="m-0 text-center text-xs" style={wideText}>
-                    {position}
-                </p>
-            </Col>
-            <Col className="d-flex flex-row justify-content-center align-items-center" xs={2} sm={1} md={1} lg={1}>
-                <div className="rounded-3" style={MyBar}></div>
-            </Col>
-            <Col className="p-0" xs={2} sm={6} md={4} lg={5}>
-                <p className="m-0 d-none d-sm-block" style={boldText}>
-                    <span style={regularText}>{firstName} </span> 
-                    {familyName}
-                </p>
-                <p className="m-0 d-block d-sm-none" style={boldText}>
-                    {code}
-                </p>
-            </Col>
-            <Col className="p-0 d-none d-md-block" md={3} lg={2}>
-                <p className="m-0 text-center" style={regularText}>
-                    {
-                        fastestLapRank === "1" ? (
-                            <span className="rounded-5 p-1 text-white" style={{backgroundColor: "purple"}}>{fastestLap}</span>
-                        ) : (
-                            <span className="rounded-5 p-1">{fastestLap}</span>
-                        )
-                    }
-                </p>
-            </Col>
-            <Col className="p-0"  xs={5} sm={3} md={2} lg={2}>
-                <p className="m-0 text-center" style={regularText}>
-                    {
-                        totalTime ? (
-                            <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{totalTime}</span>
-                        ) : ( status.includes("Lap") ? (
-                                <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{status}</span>
-                            ):(
-                                <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>DNF</span>
+        <a href="#" className="link-dark link-underline-opacity-0 link-opacity-75-hover">
+            <Row className="d-flex flex-nowrap flex-row justify-content-around align-items-center bg-white m-1 p-1 p-sm-2 rounded-3">
+                <Col className="p-0" xs={1} sm={1} md={1} lg={1}>
+                    <p className="m-0 text-center text-xs" style={wideText}>
+                        {position}
+                    </p>
+                </Col>
+                <Col className="d-flex flex-row justify-content-center align-items-center" xs={2} sm={1} md={1} lg={1}>
+                    <div className="rounded-3" style={MyBar}></div>
+                </Col>
+                <Col className="p-0" xs={2} sm={6} md={4} lg={5}>
+                    <p className="m-0 d-none d-sm-block" style={boldText}>
+                        <span style={regularText}>{firstName} </span> 
+                        {familyName}
+                    </p>
+                    <p className="m-0 d-block d-sm-none" style={boldText}>
+                        {code}
+                    </p>
+                </Col>
+                <Col className="p-0 d-none d-md-block" md={3} lg={2}>
+                    <p className="m-0 text-center" style={regularText}>
+                        {
+                            fastestLapRank === "1" ? (
+                                <span className="rounded-5 p-1 text-white" style={{backgroundColor: "purple"}}>{fastestLap}</span>
+                            ) : (
+                                <span className="rounded-5 p-1">{fastestLap}</span>
                             )
-                        )
-                    }
-                </p>
-            </Col>
-            <Col className="p-0" xs={2} sm={1} md={1} lg={1}>
-                <p className="m-0 text-center" style={blackText}>
-                    {points}
-                </p>
-            </Col>
-        </Row>
+                        }
+                    </p>
+                </Col>
+                <Col className="p-0"  xs={5} sm={3} md={2} lg={2}>
+                    <p className="m-0 text-center" style={regularText}>
+                        {
+                            totalTime ? (
+                                <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{totalTime}</span>
+                            ) : ( status.includes("Lap") ? (
+                                    <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{status}</span>
+                                ):(
+                                    <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>DNF</span>
+                                )
+                            )
+                        }
+                    </p>
+                </Col>
+                <Col className="p-0" xs={2} sm={1} md={1} lg={1}>
+                    <p className="m-0 text-center" style={blackText}>
+                        {points}
+                    </p>
+                </Col>
+            </Row>
+        </a>
     );
 }
