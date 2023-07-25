df97544aff414c356ad220d93402d1b45fa961c2
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-25 16:43:08 +0200

    Ajout des données des champions pilotes

--- src/components/champions/driver/DriverChampionsCardContainer.jsx
+++ src/components/champions/driver/DriverChampionsCardContainer.jsx
@@ -4,7 +4,15 @@ import { driversImages } from "../../../constants/driversImages";
 
 import { Card, Col, ListGroup } from "react-bootstrap";
 
-export default function DriverChampionsCardContainer(){
+export default function DriverChampionsCardContainer({
+    season,
+    driverId,
+    givenName,
+    familyName,
+    team,
+    wins,
+    points,
+}){
     const textWide = {
         fontFamily: "Formula1-Wide",
         letterSpacing: "0.0005rem",
@@ -24,28 +32,28 @@ export default function DriverChampionsCardContainer(){
         <Col className="mb-3" xs={12} sm={6} md={4} lg={3}>
             <Card className="rounded-5">
                 <Card.Header className="text-center rounded-top-5" style={{backgroundColor: "#ff1801"}}>
-                    <span className="text-white" style={textWide}>2022</span>
+                    <span className="text-white" style={textWide}>{season}</span>
                 </Card.Header>
-                <Card.Img className="mt-1" variant="top" src={driversImages["max_verstappen"]} style={{height: "150px", objectFit: "contain"}} />
+                <Card.Img className="mt-1" variant="top" src={driversImages[driverId]} style={{height: "150px", objectFit: "contain"}} />
                 <Card.Body>
                     <Card.Title className="text-center">
                         <a href="#" className="link-dark link-underline-opacity-0 link-opacity-75-hover">
-                            <span style={textRegular}>Max <br/></span>
-                            <span style={textBold}>Verstappen</span>
+                            <span style={textRegular}>{givenName} <br/></span>
+                            <span style={textBold}>{familyName}</span>
                         </a>
                     </Card.Title>
                     <ListGroup className="list-group list-group-flush">
                         <ListGroup.Item style={textBold}>
                             <a href="#" className="link-dark link-underline-opacity-0 link-opacity-75-hover fst-italic">
-                                Red Bull
+                                {team}
                             </a>
                         </ListGroup.Item>
                         <ListGroup.Item style={{fontStyle: "italic"}}>
-                            <span style={textBold}>15 </span>
+                            <span style={textBold}>{wins} </span>
                             <span style={textRegular}>wins</span>
                         </ListGroup.Item>
                         <ListGroup.Item style={{fontStyle: "italic"}}>
-                            <span style={textBold}>454 </span>
+                            <span style={textBold}>{points} </span>
                             <span style={textRegular}>points</span>
                         </ListGroup.Item>
                     </ListGroup>
--- src/components/champions/driver/DriverChampionsPage.jsx
+++ src/components/champions/driver/DriverChampionsPage.jsx
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Container, Row } from "react-bootstrap";
 import DriverChampionsCardContainer from "./DriverChampionsCardContainer";
 
+import { Container, Row, Spinner } from "react-bootstrap";
+
 export default function DriverChampionsPage() {
-    return (
-        <Container>
-            <h1 className="fst-italic" style={{fontFamily: "Formula1-Regular"}}>Driver Champions</h1>
-            <Row>
-                <DriverChampionsCardContainer />
-                <DriverChampionsCardContainer />
-                <DriverChampionsCardContainer />
-                <DriverChampionsCardContainer />
-                <DriverChampionsCardContainer />
-            </Row>
-        </Container>
-    );
+    const [champions, setChampions] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchInfo = async () => {
+        try{
+            const response = await fetch("http://ergast.com/api/f1/driverStandings/1.json?limit=100");
+            const data = await response.json();
+            setChampions(data.MRData.StandingsTable.StandingsLists);
+        }catch(error){
+            console.log(error);
+        }finally{
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchInfo();
+    }, []);
+
+    if(isLoading){
+        return(
+            <Spinner animation="border" className="align-self-center" style={{color: "#ff1801"}} />
+        );
+    }
+    else{
+        return (
+            <Container>
+                <h1 className="fst-italic" style={{fontFamily: "Formula1-Regular"}}>Driver Champions</h1>
+                <Row>
+                    {
+                        champions.map((champion, index) => {
+                            return (
+                                <DriverChampionsCardContainer
+                                    key={index}
+                                    season={champion?.season}
+                                    driverId={champion?.DriverStandings[0]?.Driver?.driverId}
+                                    givenName={champion?.DriverStandings[0]?.Driver?.givenName}
+                                    familyName={champion?.DriverStandings[0]?.Driver?.familyName}
+                                    team={champion?.DriverStandings[0]?.Constructors[0]?.name}
+                                    wins={champion?.DriverStandings[0]?.wins}
+                                    points={champion?.DriverStandings[0]?.points}
+                                />
+                            );
+                        })
+                    }
+                </Row>
+            </Container>
+        );
+    }
 }
