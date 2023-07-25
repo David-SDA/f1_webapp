a53fa0651ae01ffab75485ef085362e8bb1882fa
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-25 13:00:04 +0200

    Fin de la page classement constructeurs

--- src/components/standings/constructors/ConstructorStandingsContentContainer.jsx
+++ src/components/standings/constructors/ConstructorStandingsContentContainer.jsx
@@ -1,7 +1,85 @@
 import React from "react";
 
-export default function ConstructorStandingsContentContainer(){
+import { Col, Row } from "react-bootstrap";
+
+export default function ConstructorStandingsContentContainer({
+    position,
+    color,
+    team,
+    wins,
+    points,
+    isSelected,
+    onClick,
+    selectedTeamPoints
+}){
+    const MyBar = {
+        width: 7,
+        height: 25,
+        backgroundColor: color,
+    };
+
+    const textRegular = {
+        fontFamily: "Formula1-Regular",
+        letterSpacing: "0.0005rem",
+        color: isSelected ? "#ffffff" : "",
+    };
+
+    const textBold = {
+        fontFamily: "Formula1-Bold",
+        letterSpacing: "0.0005rem",
+        color: isSelected ? "#ffffff" : "",
+    }
+
+    const textWide = {
+        fontFamily: "Formula1-Wide",
+        letterSpacing: "0.0005rem",
+        color: isSelected ? "#ffffff" : "",
+    }
+
+    const pointsDiff = selectedTeamPoints !== 0 ? parseInt(points) - selectedTeamPoints : 0;
+
     return (
-        <p>test</p>
+        <Row className="d-flex flex-nowrap flex-row justify-content-around align-items-center m-1 mt-2 p-1 p-sm-2 rounded-3"
+            style={{backgroundColor: isSelected ? "#ff1801" : "white", cursor: "pointer"}}
+            onClick={onClick}
+        >
+            <Col className="p-0" xs={1} sm={1} md={1} lg={1}>
+                <p className="m-0 text-center" style={textWide}>
+                    {position}
+                </p>
+            </Col>
+            <Col className="d-flex flex-row justify-content-center align-items-center" xs={2} sm={1} md={1} lg={1}>
+                <div className="rounded-3" style={MyBar}></div>
+            </Col>
+            <Col className="p-0" xs={5} sm={6} md={4} lg={4}>
+                <p className="m-0 d-none d-sm-block">
+                    <a href="##" className="link-dark link-underline-opacity-0 link-opacity-75-hover">
+                        <span style={textBold}>{team}</span>
+                    </a>
+                </p>
+                <p className="m-0 d-block d-sm-none">
+                    <a href="##" className="link-dark link-underline-opacity-0 link-opacity-75-hover">
+                        <span style={{fontFamily: "Formula1-Bold", fontSize: 15, letterSpacing: "0.0005rem", color: isSelected ? "#ffffff" : ""}}>{team}</span>
+                    </a>
+                </p>
+            </Col>
+            <Col className="p-0 d-none d-md-block" md={2} lg={2}>
+                <p className="m-0 text-center" style={textBold}>
+                    {wins}
+                </p>
+            </Col>
+            <Col className="p-0" xs={2} sm={2} md={2} lg={2}>
+                <p className="m-0 text-center" style={textBold}>
+                    <span className="rounded-4 p-1 ps-2 pe-2" style={{backgroundColor: isSelected ? "#000000" : "#e8e8e8"}}>
+                        {points}
+                    </span>
+                </p>
+            </Col>
+            <Col className="p-0" xs={2} sm={2} md={2} lg={2}>
+                <p className="m-0 text-center" style={textBold}>
+                    {pointsDiff > 0 ? "+" + pointsDiff : pointsDiff}
+                </p>
+            </Col>
+        </Row>
     );
 }
--- src/components/standings/constructors/ConstructorStandingsHeaderContainer.jsx
+++ src/components/standings/constructors/ConstructorStandingsHeaderContainer.jsx
@@ -10,7 +10,7 @@ export default function ConstructorStandingsHeaderContainer(){
     
     return (
         <Row className="d-flex flex-nowrap flex-row justify-content-around align-items-center bg-white m-1 p-1 p-sm-2 rounded-3">
-            <Col className="p-0 bg-danger" xs={1} sm={1} md={1} lg={1}>
+            <Col className="p-0" xs={1} sm={1} md={1} lg={1}>
                 <p className="m-0 text-center d-none d-lg-block" style={textBlack}>
                     POSITION
                 </p>
@@ -20,19 +20,17 @@ export default function ConstructorStandingsHeaderContainer(){
             </Col>
             <Col xs={2} sm={1} md={1} lg={1}>
             </Col>
-            <Col xs={2} sm={2} md={1} lg={1}>
-            </Col>
-            <Col className="p-0 bg-danger" xs={3} sm={4} md={3} lg={3}>
+            <Col className="p-0" xs={5} sm={6} md={4} lg={4}>
                 <p className="m-0" style={textBlack}>
                     TEAM
                 </p>
             </Col>
-            <Col className="p-0 d-none d-md-none bg-info" md={2} lg={2}>
+            <Col className="p-0 d-none d-md-block" md={2} lg={2}>
                 <p className="m-0 text-center" style={textBlack}>
                     WINS
                 </p>
             </Col>
-            <Col className="p-0 bg-danger" xs={2} sm={2} md={2} lg={2}>
+            <Col className="p-0" xs={2} sm={2} md={2} lg={2}>
                 <p className="m-0 text-center d-none d-lg-block" style={textBlack}>
                     POINTS
                 </p>
@@ -40,7 +38,7 @@ export default function ConstructorStandingsHeaderContainer(){
                     PTS
                 </p>
             </Col>
-            <Col className="p-0 bg-info" xs={2} sm={2} md={2} lg={2}>
+            <Col className="p-0" xs={2} sm={2} md={2} lg={2}>
                 <p className="m-0 text-center" style={textBlack}>
                     DIFF
                 </p>
