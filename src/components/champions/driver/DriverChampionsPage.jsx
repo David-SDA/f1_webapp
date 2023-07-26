f84c9b85ba7203285794c5bfbb7415c39f5d0c4f
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-26 12:35:10 +0200

    Ajustement du style du filtre

--- src/components/champions/driver/DriverChampionsPage.jsx
+++ src/components/champions/driver/DriverChampionsPage.jsx
@@ -79,23 +79,29 @@ export default function DriverChampionsPage() {
             <Container>
                 <h1 className="fst-italic" style={{fontFamily: "Formula1-Regular"}}>Driver Champions</h1>
                 <Row className="d-flex flex-row align-items-center mb-3">
-                    <Col lg={8}>
+                    <Col xs={12} sm={7} md={7} lg={8} className="mb-2">
                         <FormControl type="text" placeholder="Search by family name" className="mr-sm-2" />
                     </Col>
-                    <Col lg={2}>
-                        <Form.Select value={filter} onChange={handleFilterOptionChange}>
+                    <Col xs={10} sm={4} md={4} lg={3} className="mb-2">
+                        <Form.Select value={filter} onChange={handleFilterOptionChange} style={{fontFamily: "Formula1-Regular"}}>
                             <option value="season">Season</option>
                             <option value="DriverStandings[0].Driver.givenName">First Name</option>
                             <option value="DriverStandings[0].Driver.familyName">Last Name</option>
                             <option value="DriverStandings[0].Constructors[0].name">Constructor</option>
                         </Form.Select>
                     </Col>
-                    <Col lg={2}>
+                    <Col xs={1} sm={1} md={1} lg={1} className="mb-2 text-end">
                         {
                             sortOrder === "ascending" ? (
-                                <span onClick={handleSortChange} style={{cursor: "pointer"}}>&#x25B2; Ascending</span>
+                                <span className="d-flex flex-column user-select-none" onClick={handleSortChange} style={{cursor: "pointer"}}>
+                                    <span className="opacity-100">&#x25B2;</span>
+                                    <span className="opacity-25">&#x25BC;</span>
+                                </span>
                             ) : (
-                                <span onClick={handleSortChange} style={{cursor: "pointer"}}>&#x25BC; Descending</span>
+                                <span className="d-flex flex-column user-select-none" onClick={handleSortChange} style={{cursor: "pointer"}}>
+                                    <span className="opacity-25">&#x25B2;</span>
+                                    <span className="opacity-100">&#x25BC;</span>
+                                </span>
                             )
                         }
                     </Col>
