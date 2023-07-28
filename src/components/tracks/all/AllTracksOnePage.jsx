e9133e4e7dc76c58c949620e4b55ff7f512da3a1
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-28 11:11:21 +0200

    Ajout des premières statistiques sur le circuit

--- src/components/tracks/all/AllTracksOnePage.jsx
+++ src/components/tracks/all/AllTracksOnePage.jsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from "react";
 
+import { allTracks } from "../../../constants/allTracks";
+import { flags } from "../../../constants/flags";
+
 import { Col, Container, Image, Row, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { allTracks } from "../../../constants/allTracks";
 
 export default function AllTracksOnePage(){
     let { circuitId } = useParams();
 
     const [circuit, setCircuit] = useState([]);
+    const [winners, setWinners] = useState([]);
+    const [races, setRaces] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     const fetchInfo = async () => {
         try{
-            const response = await fetch("http://ergast.com/api/f1/circuits/monza.json")
-            const data = await response.json();
-            setCircuit(data.MRData.CircuitTable.Circuits[0])
+            const [response1, response2, response3] = await Promise.all([
+                fetch("http://ergast.com/api/f1/circuits/"+ circuitId + ".json"),
+                fetch("http://ergast.com/api/f1/circuits/"+ circuitId + "/results/1.json?limit=100"),
+                fetch("http://ergast.com/api/f1/circuits/"+ circuitId + "/races.json?limit=100"),
+            ]);
+            
+            const data1 = await response1.json();
+            const data2 = await response2.json();
+            const data3 = await response3.json();
+
+            setCircuit(data1.MRData.CircuitTable.Circuits[0]);
+            setWinners(data2.MRData.RaceTable.Races);
+            setRaces(data3.MRData.RaceTable.Races);
         }catch(error){
             console.log(error);
         }finally{
@@ -25,6 +39,23 @@ export default function AllTracksOnePage(){
     useEffect(() => {
         fetchInfo();
     }, [])
+
+    const driversWinsCount = winners.reduce((winsCount, race) => {
+        const winnerName = race?.Results[0]?.Driver.givenName + " " + race?.Results[0]?.Driver.familyName;
+
+        if(winsCount[winnerName]){
+            winsCount[winnerName]++;
+        }
+        else{
+            winsCount[winnerName] = 1;
+        }
+
+        return winsCount;
+    }, {});
+
+    const countDifferentWinners = () => {
+        return Object.keys(driversWinsCount).length;
+    }
 
     const textBlack = {
         fontFamily: "Formula1-Black",
@@ -50,21 +81,42 @@ export default function AllTracksOnePage(){
         return (
             <Container>
                 <h1 className="fst-italic" style={textRegular}>{circuit?.circuitName}</h1>
-                <Row>
+                <Row className="d-flex align-items-center">
                     <Col lg={6} className="d-flex justify-content-center align-items-center">
                         <Image src={allTracks[circuitId]} style={{height: "300px", objectFit: "contain"}}/>
                     </Col>
-                    <Col lg={6} className="bg-warning">
+                    <Col lg={6}>
                         <Row>
-                            <Col lg={6}>
+                            <Col lg={6} className="mb-3">
                                 <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
                                     <span style={textBlack}>COUNTRY</span>
-                                    <span className="text-center" style={textBold}>{circuit?.Location?.country}</span>
+                                    <Container className="d-flex justify-content-center align-items-center">
+                                        <Image src={flags[circuit?.Location?.country]} className="rounded border me-2" style={{height: "25px"}} />
+                                        <span className="text-center" style={textBold}>{circuit?.Location?.country}</span>
+                                    </Container>
                                     <div></div>
                                 </Container>
                             </Col>
-                            <Col lg={6} className="bg-secondary">
-                                <span>{circuit?.Location?.locality}</span>
+                            <Col lg={6} className="mb-3">
+                                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                                    <span style={textBlack}>LOCALITY</span>
+                                    <span className="text-center" style={textBold}>{circuit?.Location?.locality}</span>
+                                    <div></div>
+                                </Container>
+                            </Col>
+                            <Col lg={6} className="mb-3">
+                                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                                    <span style={textBlack}>RACES</span>
+                                    <span className="text-center" style={textBold}>{races.length}</span>
+                                    <div></div>
+                                </Container>
+                            </Col>
+                            <Col lg={6} className="mb-3">
+                                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                                    <span style={textBlack}>WINNERS</span>
+                                    <span className="text-center" style={textBold}>{countDifferentWinners()}</span>
+                                    <div></div>
+                                </Container>
                             </Col>
                         </Row>
                     </Col>
