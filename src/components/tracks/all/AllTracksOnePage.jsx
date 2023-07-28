fa3918f7a201cfbb3d0d04699a90361085e95389
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-28 15:50:42 +0200

    Ajout de la liste des courses d'un circuit

--- src/components/tracks/all/AllTracksOnePage.jsx
+++ src/components/tracks/all/AllTracksOnePage.jsx
@@ -14,6 +14,7 @@ export default function AllTracksOnePage(){
     const [races, setRaces] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [showAllWinners, setShowAllWinners] = useState(false);
+    const [showAllRaces, setShowAllRaces] = useState(false);
 
     const fetchInfo = async () => {
         try{
@@ -67,8 +68,12 @@ export default function AllTracksOnePage(){
         return Object.keys(driversWinsCount).length;
     }
 
-    const handleDisplayMore = () => {
+    const handleDisplayMoreWinners = () => {
         setShowAllWinners(!showAllWinners);
+    }
+
+    const handleDisplayMoreRaces = () => {
+        setShowAllRaces(!showAllRaces);
     }
 
     const sortedWinners = Object.entries(driversWinsCount).sort((a, b) => b[1].wins - a[1].wins);
@@ -136,8 +141,8 @@ export default function AllTracksOnePage(){
                             </Col>
                         </Row>
                     </Col>
-                    <Col lg={12}>
-                        <h2 className="fst-italic" style={textRegular}>All winners</h2>
+                    <Col lg={12} className="mb-3">
+                        <p className="h2 fst-italic" style={textRegular}>All winners</p>
                         <Container className="d-flex flex-column p-1 rounded-3" style={{backgroundColor: "#38383f"}}>
                             {
                                 sortedWinners.slice(0, showAllWinners ? sortedWinners.length : 5).map(([driver, { wins, years}]) => (
@@ -159,8 +164,65 @@ export default function AllTracksOnePage(){
                             }
                             {
                                 countDifferentWinners() > 5 ? (
-                                    <Button variant="outline-light" size="sm" className="align-self-end mt-1" onClick={handleDisplayMore}>
+                                    <Button variant="outline-light" size="sm" className="align-self-center mt-1" onClick={handleDisplayMoreWinners}>
                                         {showAllWinners ? "Show less" : "Show More"}
+                                    </Button>
+                                ) : ("")
+                            }
+                        </Container>
+                    </Col>
+                    <Col lg={12}>
+                        <p className="h2 fst-italic" style={textRegular}>All Races</p>
+                        <Container className="d-flex flex-column p-1 rounded-3" style={{backgroundColor: "#38383f"}}>
+                            <Row className="bg-white m-1 p-1 w-auto rounded-3">
+                                <Col lg={3}>
+                                    <p className="mb-0 text-center" style={textBlack}>
+                                        SEASON
+                                    </p>
+                                </Col>
+                                <Col lg={6}>
+                                    <p className="mb-0 text-center" style={textBlack}>
+                                        GRAND PRIX NAME
+                                    </p>
+                                </Col>
+                                <Col lg={3}>
+                                    <p className="mb-0 text-center" style={textBlack}>
+                                        DATE
+                                    </p>
+                                </Col>
+                            </Row>
+                            {
+                                races.slice().reverse().slice(0, showAllRaces ? races.length : 5).map((race, index) => {
+                                    let raceDate = new Date(race?.date);
+                                    return (
+                                        <Row key={index} className="bg-white m-1 p-2 w-auto rounded-3">
+                                            <Col lg={3}>
+                                                <p className="mb-0 text-center" style={textBold}>
+                                                    <a href="#" className="link-dark link-underline-opacity-0 link-opacity-50-hover">
+                                                        {race?.season}
+                                                    </a>
+                                                </p>
+                                            </Col>
+                                            <Col lg={6}>
+                                                <p className="mb-0 text-center" style={textBold}>
+                                                    <a href="#" className="link-dark link-underline-opacity-0 link-opacity-50-hover">
+                                                        {race?.raceName}
+                                                    </a>
+                                                </p>
+                                            </Col>
+                                            <Col lg={3}>
+                                                <p className="mb-0 text-center" style={textRegular}>
+                                                    {raceDate.toLocaleDateString("en-GB", {day: "2-digit", month: "short"})}
+                                                </p>
+                                            </Col>
+                                        </Row>
+                                    );
+                                })
+                            }
+                            {
+                                races.length > 5 ? (
+                                    <Button variant="outline-light" size="sm" className="align-self-center mt-1" onClick={handleDisplayMoreRaces}>
+                                        {showAllRaces ? "Show less" : "Show More"}
                                     </Button>
                                 ) : ("")
                             }
