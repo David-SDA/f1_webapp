7664e0fd3ca099dca525c02ecb08a6ff9d3c0f12
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-01 14:42:40 +0200

    Séparation en composant

---
+++ src/components/drivers/current/thisSeasonRaces/CurrentDriversThisSeasonRacesContentContainer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+export default function CurrentDriversThisSeasonRacesContentContainer({
+    round,
+    raceName,
+    grid,
+    position,
+    time,
+    status,
+    points,
+}){
+    const textRegular = {
+        fontFamily: "Formula1-Regular",
+        letterSpacing: "0.0005rem",
+    }
+
+    const textBold = {
+        fontFamily: "Formula1-Bold",
+        letterSpacing: "0.0005rem",
+    }
+
+    const textBlack = {
+        fontFamily: "Formula1-Black",
+        letterSpacing: "0.0005rem",
+    }
+    return (
+        <Row className="bg-white ms-1 me-1 mb-2 p-1 rounded">
+            <Col sm={1} md={1} lg={1} className="d-none d-sm-flex justify-content-center align-items-center p-0">
+                <p className="mb-0 text-center" style={textBlack}>{round}</p>
+            </Col>
+            <Col xs={6} sm={7} md={6} lg={4} className="d-flex justify-content-center align-items-center p-0">
+                <a href="#" className="link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
+                    <p className="d-flex d-sm-none align-items-center mb-0 text-center" style={{...textBold, minHeight: "3em"}}>{raceName}</p>
+                    <p className="d-none d-sm-flex align-items-center mb-0 text-center" style={textBold}>{raceName}</p>
+                </a>
+            </Col>
+            <Col md={1} lg={1} className="d-flex justify-content-center align-items-center d-none d-md-block p-0">
+                <p className="mb-0 text-center" style={textBold}>{grid}</p>
+            </Col>
+            <Col xs={2} sm={1} md={1} lg={1} className="d-flex justify-content-center align-items-center p-0">
+                <p className="mb-0 text-center" style={textBold}>{position}</p>
+            </Col>
+            <Col xs={4} sm={3} md={2} lg={4} className="d-flex justify-content-center align-items-center p-0">
+                {
+                    time ? (
+                        <p className="mb-0 text-center" style={textRegular}>
+                            <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{time}</span>
+                        </p>
+                    ) : (
+                        <p className="mb-0 text-center" style={textRegular}>
+                            <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{status}</span>
+                        </p>
+                    )
+                }
+            </Col>
+            <Col md={1} lg={1} className="d-flex justify-content-center align-items-center d-none d-md-block p-0">
+                <p className="mb-0 text-center" style={textBlack}>{points}</p>
+            </Col>
+        </Row>
+    );
+}
---
+++ src/components/drivers/current/thisSeasonRaces/CurrentDriversThisSeasonRacesHeaderContainer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { Col, Row } from "react-bootstrap";
+
+export default function CurrentDriversThisSeasonRacesHeaderContainer(){
+    const textRegular = {
+        fontFamily: "Formula1-Regular",
+        letterSpacing: "0.0005rem",
+    }
+
+    const textBold = {
+        fontFamily: "Formula1-Bold",
+        letterSpacing: "0.0005rem",
+    }
+
+    const textBlack = {
+        fontFamily: "Formula1-Black",
+        letterSpacing: "0.0005rem",
+    }
+    
+    return (
+        <Row className="bg-white mt-2 mt-sm-0 ms-1 me-1 mb-2 p-1 rounded">
+            <Col sm={1} md={1} lg={1} className="d-none d-sm-block p-0">
+                <p className="d-none d-md-block mb-0 text-center" style={textBlack}>ROUND</p>
+                <p className="d-block d-md-none mb-0 text-center" style={textBlack}>R</p>
+            </Col>
+            <Col xs={6} sm={7} md={6} lg={4} className="p-0">
+                <p className="mb-0 text-center" style={textBlack}>GRAND PRIX</p>
+            </Col>
+            <Col md={1} lg={1} className="d-none d-md-block p-0">
+                <p className="mb-0 text-center" style={textBlack}>GRID</p>
+            </Col>
+            <Col xs={2} sm={1} md={1} lg={1} className="p-0">
+                <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>POSITION</p>
+                <p className="d-none d-sm-block d-lg-none mb-0 text-center" style={textBlack}>POS</p>
+                <p className="d-block d-sm-none mb-0 text-center" style={textBlack}>P</p>
+            </Col>
+            <Col xs={4} sm={3} md={2} lg={4} className="p-0">
+                <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>TIME/STATUS</p>
+                <p className="d-block d-lg-none mb-0 text-center" style={textBlack}>TIME</p>
+            </Col>
+            <Col md={1} lg={1} className="d-none d-md-block p-0">
+                <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>POINTS</p>
+                <p className="d-block d-lg-none mb-0 text-center" style={textBlack}>PTS</p>
+            </Col>
+        </Row>
+    );
+}
