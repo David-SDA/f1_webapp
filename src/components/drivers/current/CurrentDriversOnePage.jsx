0384f662c47bcc29eaa853e072476c749270a255
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-01 12:55:50 +0200

    Ajout courses couru d'un pilote cette saison

--- src/components/drivers/current/CurrentDriversOnePage.jsx
+++ src/components/drivers/current/CurrentDriversOnePage.jsx
@@ -94,6 +94,73 @@ export default function CurrentDriversOnePage(){
                     wins={standing?.DriverStandings[0]?.wins}
                     nbPodiums={nbPodiums}
                 />
+                <h2 className="fst-italic mt-2" style={textRegular}>RACES</h2>
+                <Container className="d-flex flex-column mb-2 p-0 p-sm-2 rounded" style={{backgroundColor: "#38383f"}}>
+                    <Row className="bg-white mt-2 mt-sm-0 ms-1 me-1 mb-2 p-1 rounded">
+                        <Col sm={1} md={1} lg={1} className="d-none d-sm-block p-0">
+                            <p className="d-none d-md-block mb-0 text-center" style={textBlack}>ROUND</p>
+                            <p className="d-block d-md-none mb-0 text-center" style={textBlack}>R</p>
+                        </Col>
+                        <Col xs={6} sm={7} md={6} lg={4} className="p-0">
+                            <p className="mb-0 text-center" style={textBlack}>GRAND PRIX</p>
+                        </Col>
+                        <Col md={1} lg={1} className="d-none d-md-block p-0">
+                            <p className="mb-0 text-center" style={textBlack}>GRID</p>
+                        </Col>
+                        <Col xs={2} sm={1} md={1} lg={1} className="p-0">
+                            <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>POSITION</p>
+                            <p className="d-none d-sm-block d-lg-none mb-0 text-center" style={textBlack}>POS</p>
+                            <p className="d-block d-sm-none mb-0 text-center" style={textBlack}>P</p>
+                        </Col>
+                        <Col xs={4} sm={3} md={2} lg={4} className="p-0">
+                            <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>TIME/STATUS</p>
+                            <p className="d-block d-lg-none mb-0 text-center" style={textBlack}>TIME</p>
+                        </Col>
+                        <Col md={1} lg={1} className="d-none d-md-block p-0">
+                            <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>POINTS</p>
+                            <p className="d-block d-lg-none mb-0 text-center" style={textBlack}>PTS</p>
+                        </Col>
+                    </Row>
+                    {
+                        [...driverResults].reverse().map((driverResult, index) => {
+                            return (
+                                <Row className="bg-white ms-1 me-1 mb-2 p-1 rounded" key={index}>
+                                    <Col sm={1} md={1} lg={1} className="d-none d-sm-flex justify-content-center align-items-center p-0">
+                                        <p className="mb-0 text-center" style={textBlack}>{driverResult?.round}</p>
+                                    </Col>
+                                    <Col xs={6} sm={7} md={6} lg={4} className="d-flex justify-content-center align-items-center p-0">
+                                        <a href="#" className="link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
+                                            <p className="d-flex d-sm-none align-items-center mb-0 text-center" style={{...textBold, minHeight: "3em"}}>{driverResult?.raceName}</p>
+                                            <p className="d-none d-sm-flex align-items-center mb-0 text-center" style={textBold}>{driverResult?.raceName}</p>
+                                        </a>
+                                    </Col>
+                                    <Col md={1} lg={1} className="d-flex justify-content-center align-items-center d-none d-md-block p-0">
+                                        <p className="mb-0 text-center" style={textBold}>{driverResult?.Results[0]?.grid}</p>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} lg={1} className="d-flex justify-content-center align-items-center p-0">
+                                        <p className="mb-0 text-center" style={textBold}>{driverResult?.Results[0]?.positionText}</p>
+                                    </Col>
+                                    <Col xs={4} sm={3} md={2} lg={4} className="d-flex justify-content-center align-items-center p-0">
+                                        {
+                                            driverResult?.Results[0]?.Time ? (
+                                                <p className="mb-0 text-center" style={textRegular}>
+                                                    <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{driverResult?.Results[0]?.Time?.time}</span>
+                                                </p>
+                                            ) : (
+                                                <p className="mb-0 text-center" style={textRegular}>
+                                                    <span className="rounded-5 p-1" style={{backgroundColor: "#e8e8e8"}}>{driverResult?.Results[0]?.status}</span>
+                                                </p>
+                                            )
+                                        }
+                                    </Col>
+                                    <Col md={1} lg={1} className="d-flex justify-content-center align-items-center d-none d-md-block p-0">
+                                        <p className="mb-0 text-center" style={textBlack}>{driverResult?.Results[0]?.points}</p>
+                                    </Col>
+                                </Row>
+                            );
+                        })
+                    }
+                </Container>
             </Container>
         );
     }
