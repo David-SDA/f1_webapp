14ef2a27696f651b3551da640f2450e523977fb0
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-02 10:47:25 +0200

    Séparation en composant

---
+++ src/components/constructors/current/CurrentConstructorsCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Card, Col, Image } from "react-bootstrap";
+
+export default function CurrentConstructorsCard({
+    color,
+    imageFlag,
+    name,
+    imageTeam,
+}){
+    const textBold = {
+        fontFamily: "Formula1-Bold",
+        letterSpacing: "0.0005rem",
+    }
+    
+    return (
+        <Col md={6} lg={4} xl={3} className="mb-2">
+            <Card className="rounded-5 overflow-hidden" style={{height: "240px"}}>
+                <a href="#" className="link-light link-underline-opacity-0 link-underline-opacity-100-hover">
+                    <Card.Header className="d-flex flex-row justify-content-center align-items-center text-white rounded-top-5 pt-3 pb-3" style={{backgroundColor: color}}>
+                        <Image src={imageFlag} rounded className="me-1 border" style={{height: 25}} />
+                        <p className="mb-0" style={textBold}>{name}</p>
+                    </Card.Header>
+                    <Card.Body className="d-flex flex-row justify-content-center align-items-center">
+                        <Image src={imageTeam} className="rounded-0" style={{height: "150px", objectFit: "cover"}} />
+                    </Card.Body>
+                </a>
+            </Card>
+        </Col>
+    );
+}
--- src/components/constructors/current/CurrentConstructorsPage.jsx
+++ src/components/constructors/current/CurrentConstructorsPage.jsx
@@ -5,6 +5,7 @@ import { flagsNationality } from "../../../constants/flagsNationality";
 import { currentConstructorImage } from "../../../constants/currentConstructorImage";
 
 import { Card, Col, Container, Image, Row, Spinner } from "react-bootstrap";
+import CurrentConstructorsCard from "./CurrentConstructorsCard";
 
 export default function CurrentConstructorsPage(){
     const [constructors, setConstructors] = useState([]);
@@ -31,16 +32,6 @@ export default function CurrentConstructorsPage(){
         letterSpacing: "0.0005rem",
     }
 
-    const textBold = {
-        fontFamily: "Formula1-Bold",
-        letterSpacing: "0.0005rem",
-    }
-
-    const textBlack = {
-        fontFamily: "Formula1-Black",
-        letterSpacing: "0.0005rem",
-    }
-
     if(isLoading){
         return(
             <Spinner animation="border" style={{color: "#ff1801"}} />
@@ -54,19 +45,13 @@ export default function CurrentConstructorsPage(){
                     {
                         constructors.map((constructor, index) => {
                             return (
-                                <Col md={6} lg={4} xl={3} className="mb-2" key={index}>
-                                    <Card className="rounded-5 overflow-hidden" style={{height: "240px"}}>
-                                        <a href="#" className="link-light link-underline-opacity-0 link-underline-opacity-100-hover">
-                                            <Card.Header className="d-flex flex-row justify-content-center align-items-center text-white rounded-top-5 pt-3 pb-3" style={{backgroundColor: currentConstructorColor[constructor?.constructorId]}}>
-                                                <Image src={flagsNationality[constructor?.nationality]} rounded className="me-1 border" style={{height: 25}} />
-                                                <p className="mb-0" style={textBold}>{constructor?.name}</p>
-                                            </Card.Header>
-                                            <Card.Body className="d-flex flex-row justify-content-center align-items-center">
-                                                <Image src={currentConstructorImage[constructor?.constructorId]} className="rounded-0" style={{height: "150px", objectFit: "cover"}} />
-                                            </Card.Body>
-                                        </a>
-                                    </Card>
-                                </Col>
+                                <CurrentConstructorsCard
+                                    key={index}
+                                    color={currentConstructorColor[constructor?.constructorId]}
+                                    imageFlag={flagsNationality[constructor?.nationality]}
+                                    name={constructor?.name}
+                                    imageTeam={currentConstructorImage[constructor?.constructorId]}
+                                />
                             );
                         })
                     }
