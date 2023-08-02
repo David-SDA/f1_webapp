668c60ba52ad75e4c2cfd50db5693a260ddf11bf
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-02 17:00:49 +0200

    Début de tentative de résolution du cas 3 pilotes

--- src/components/constructors/current/thisSeasonRacesSprint/CurrentConstructorsThisSeasonRacesSprintContentContainer.jsx
+++ src/components/constructors/current/thisSeasonRacesSprint/CurrentConstructorsThisSeasonRacesSprintContentContainer.jsx
@@ -7,6 +7,7 @@ export default function CurrentConstructorsThisSeasonRacesSprintContentContainer
     race,
     driver1Position,
     driver2Position,
+    driver3Position,
     points,
 }){
     const textBold = {
@@ -23,16 +24,43 @@ export default function CurrentConstructorsThisSeasonRacesSprintContentContainer
             <Col sm={1} md={1} lg={1} className="d-none d-sm-block p-0">
                 <p className="mb-0 text-center" style={textBlack}>{round}</p>
             </Col>
-            <Col xs={8} sm={7} md={6} lg={6} className="d-flex justify-content-center p-0">
-                <p className="d-flex d-sm-none align-items-center mb-0 text-center" style={{...textBold, minHeight: "3em"}}>{race}</p>
-                <p className="d-none d-sm-flex align-items-center mb-0 text-center" style={textBold}>{race}</p>
-            </Col>
-            <Col xs={2} sm={2} md={2} lg={2} className="p-0">
-                <p className="mb-0 text-center" style={textBold}>{driver1Position}</p>
-            </Col>
-            <Col xs={2} sm={2} md={2} lg={2} className="p-0">
-                <p className="mb-0 text-center" style={textBold}>{driver2Position}</p>
-            </Col>
+            {
+                driver3Position === null ? (
+                    <Col xs={6} sm={5} md={4} lg={4} className="d-flex justify-content-center p-0">
+                        <p className="d-flex d-sm-none align-items-center mb-0 text-center" style={{...textBold, minHeight: "3em"}}>{race}</p>
+                        <p className="d-none d-sm-flex align-items-center mb-0 text-center" style={textBold}>{race}</p>
+                    </Col>
+                ) : (
+                    <Col xs={8} sm={7} md={6} lg={6} className="d-flex justify-content-center p-0">
+                        <p className="d-flex d-sm-none align-items-center mb-0 text-center" style={{...textBold, minHeight: "3em"}}>{race}</p>
+                        <p className="d-none d-sm-flex align-items-center mb-0 text-center" style={textBold}>{race}</p>
+                    </Col>
+                )
+            }
+            {
+                driver3Position === null ? (
+                    <>
+                        <Col xs={2} sm={2} md={2} lg={2} className="p-0">
+                            <p className="mb-0 text-center" style={textBold}>{driver1Position}</p>
+                        </Col>
+                        <Col xs={2} sm={2} md={2} lg={2} className="p-0">
+                            <p className="mb-0 text-center" style={textBold}>{driver2Position}</p>
+                        </Col>
+                        <Col xs={2} sm={2} md={2} lg={2} className="p-0">
+                            <p className="mb-0 text-center" style={textBold}>{driver3Position}</p>
+                        </Col>
+                    </>
+                ) : (
+                    <>
+                        <Col xs={2} sm={2} md={2} lg={2} className="p-0">
+                            <p className="mb-0 text-center" style={textBold}>{driver1Position}</p>
+                        </Col>
+                        <Col xs={2} sm={2} md={2} lg={2} className="p-0">
+                            <p className="mb-0 text-center" style={textBold}>{driver2Position}</p>
+                        </Col>
+                    </>
+                )
+            }
             <Col md={1} lg={1} className="d-none d-md-block p-0">
                 <p className="mb-0 text-center" style={textBlack}>{points}</p>
             </Col>
