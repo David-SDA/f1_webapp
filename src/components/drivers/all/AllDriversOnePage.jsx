381b6b5fc605f913f284e3b961cd33759b15d685
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-04 15:14:05 +0200

    Ajout des courses d'un pilote

--- src/components/drivers/all/AllDriversOnePage.jsx
+++ src/components/drivers/all/AllDriversOnePage.jsx
@@ -151,6 +151,58 @@ export default function AllDriversOnePage(){
                         }
                     </Row>
                 </Container>
+                <h1 className="fst-italic mt-1" style={textRegular}>
+                    RACES
+                </h1>
+                <Container className="mb-2 pt-3 pb-3 ps-1 pe-1 rounded" style={{backgroundColor: "#38383f"}}>
+                    <Row className="bg-white mt-sm-0 ms-1 me-1 p-1 rounded">
+                        <Col sm={1} md={1} lg={1} className="d-none d-sm-block p-0">
+                            <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>ROUND</p>
+                            <p className="d-block d-lg-none mb-0 text-center" style={textBlack}>R</p>
+                        </Col>
+                        <Col xs={2} sm={3} md={2} lg={2} className="ps-0 ps-sm-2 pe-0 pe-sm-2">
+                            <p className="d-none d-sm-block mb-0 text-center" style={textBlack}>SEASON</p>
+                            <p className="d-block d-sm-none mb-0 text-center" style={textBlack}>2023</p>
+                        </Col>
+                        <Col xs={8} sm={7} md={7} lg={6}>
+                            <p className="mb-0 text-center" style={textBlack}>RACE</p>
+                        </Col>
+                        <Col md={1} lg={1} className="d-none d-md-block p-0">
+                            <p className="mb-0 text-center" style={textBlack}>GRID</p>
+                        </Col>
+                        <Col xs={2} sm={1} md={1} lg={2} className="p-0">
+                            <p className="d-none d-lg-block mb-0 text-center" style={textBlack}>POSITION</p>
+                            <p className="d-none d-sm-block d-lg-none mb-0 text-center" style={textBlack}>POS</p>
+                            <p className="d-block d-sm-none mb-0 text-center" style={textBlack}>P</p>
+                        </Col>
+                    </Row>
+                    {
+                        [...results].reverse().map((result, index) => {
+                            return (
+                                <Row className="bg-white mt-2 ms-1 me-1 p-1 rounded" key={index}>
+                                    <Col sm={1} md={1} lg={1} className="d-none d-sm-block p-0">
+                                        <p className="mb-0 text-center" style={textBlack}>{result?.round}</p>
+                                    </Col>
+                                    <Col xs={2} sm={3} md={2} lg={2} className="d-flex justify-content-center align-items-center ps-0 ps-sm-2 pe-0 pe-sm-2">
+                                        <p className="mb-0 text-center" style={textRegular}>{result?.season}</p>
+                                    </Col>
+                                    <Col xs={8} sm={7} md={7} lg={6} className="d-flex justify-content-center align-items-center">
+                                        <a href="#" className="link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
+                                            <p className="d-flex d-sm-none align-items-center mb-0 text-center" style={{...textBold, minHeight: "3em"}}>{result?.raceName}</p>
+                                            <p className="d-none d-sm-flex align-items-center mb-0 text-center" style={textBold}>{result?.raceName}</p>
+                                        </a>
+                                    </Col>
+                                    <Col md={1} lg={1} className="d-none d-md-block p-0">
+                                        <p className="mb-0 text-center" style={textBlack}>{result?.Results[0]?.grid}</p>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} lg={2} className="d-flex justify-content-center align-items-center p-0">
+                                        <p className="mb-0 text-center" style={textBlack}>{result?.Results[0]?.positionText}</p>
+                                    </Col>
+                                </Row>
+                            );
+                        })
+                    }
+                </Container>
             </Container>
         );
     }
