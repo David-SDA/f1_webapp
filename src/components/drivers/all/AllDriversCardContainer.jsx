455613f23c63f9b8ff7fee599aa3d597fcf0061f
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-04 09:58:02 +0200

    Ajout de drapeaux manquants et ajustements

--- src/components/drivers/all/AllDriversCardContainer.jsx
+++ src/components/drivers/all/AllDriversCardContainer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Card, Col } from "react-bootstrap";
+
+import { flagsNationality } from "../../../constants/flagsNationality";
+
+import { Card, Col, Image } from "react-bootstrap";
 
 export default function AllDriversCardContainer({
     driverId,
@@ -16,14 +19,15 @@ export default function AllDriversCardContainer({
         letterSpacing: "0.0005rem",
     }
     return (
-        <Col sm={6} md={4} lg={2} className="mb-2 p-1">
+        <Col sm={6} md={4} lg={3} xl={2} className="mb-2 p-1">
             <a href={"/allDrivers/" + driverId} className="link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
                 <Card style={{height: "8rem"}}>
                     <Card.Body>
                         <Card.Title>
                             <span style={textRegular}>{givenName}</span> <span style={textBold}>{familyName}</span>
                         </Card.Title>
-                        <Card.Subtitle className="link-underline-opacity-0-hover">
+                        <Card.Subtitle className="d-flex align-items-center">
+                            <Image src={flagsNationality[nationality]} rounded className="me-1 border" style={{height: 20}} />
                             <span className="fst-italic" style={textRegular}>{nationality}</span>
                         </Card.Subtitle>
                     </Card.Body>
