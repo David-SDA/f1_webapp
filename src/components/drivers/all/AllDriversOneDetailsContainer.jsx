dc3bbc9a8d2d79e50cd80fd2de3943d9e93b225b
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-04 10:51:41 +0200

    Séparation des détails d'un pilote en composant

---
+++ src/components/drivers/all/AllDriversOneDetailsContainer.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+import { flagsNationality } from "../../../constants/flagsNationality";
+
+import { Col, Container, Image, Row } from "react-bootstrap";
+
+export default function AllDriversOneDetailsContainer({
+    nationality,
+    dateOfBirth,
+    firstSeason,
+    firstRace,
+    nbSeasons,
+    nbRaces,
+    nbChampionships,
+    nbWins,
+    nbPodiums,
+    nbTeams,
+}){
+    const textBold = {
+        fontFamily: "Formula1-Bold",
+        letterSpacing: "0.0005rem",
+    }
+
+    const textBlack = {
+        fontFamily: "Formula1-Black",
+        letterSpacing: "0.0005rem",
+    }
+
+    return (
+        <Row className="d-flex justify-content-center">
+            <Col md={6} lg={4}>
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>NATIONALITY</p>
+                    <div className="d-flex flex-row justify-content-center align-items-center">
+                        <Image src={flagsNationality[nationality]} rounded className="me-1 border" style={{height: 25}} />
+                        <p className="mb-0" style={{...textBold, fontSize: "20px"}}>{nationality}</p>
+                    </div>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>DATE OF BIRTH</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{new Date(dateOfBirth).toLocaleDateString("en")}</p>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>FIRST RACE</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{firstSeason} {firstRace}</p>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>COMPLETED SEASONS</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbSeasons}</p>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>RACES</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbRaces}</p>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>WORLD CHAMPIONSHIPS</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbChampionships}</p>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>WINS</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbWins}</p>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>PODIUMS</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbPodiums}</p>
+                    <div></div>
+                </Container>
+            </Col>
+            <Col md={6} lg={4} className="mb-3">
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>TEAMS</p>
+                    <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbTeams}</p>
+                    <div></div>
+                </Container>
+            </Col>
+        </Row>
+    );
+}
--- src/components/drivers/all/AllDriversOnePage.jsx
+++ src/components/drivers/all/AllDriversOnePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { flagsNationality } from "../../../constants/flagsNationality";
+import AllDriversOneDetailsContainer from "./AllDriversOneDetailsContainer";
 
 import { Col, Container, Image, Row, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
@@ -107,74 +107,18 @@ export default function AllDriversOnePage(){
                 <h1 className="fst-italic mt-1" style={textRegular}>
                     <span style={textRegular}>{driver?.givenName}</span> <span style={textBold}>{driver?.familyName}</span>
                 </h1>
-                <Row className="d-flex justify-content-center">
-                    <Col md={6} lg={4}>
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>NATIONALITY</p>
-                            <div className="d-flex flex-row justify-content-center align-items-center">
-                                <Image src={flagsNationality[driver?.nationality]} rounded className="me-1 border" style={{height: 25}} />
-                                <p className="mb-0" style={{...textBold, fontSize: "20px"}}>{driver?.nationality}</p>
-                            </div>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>DATE OF BIRTH</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{new Date(driver?.dateOfBirth).toLocaleDateString("en")}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>FIRST RACE</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{results[0]?.season} {results[0]?.raceName}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>COMPLETED SEASONS</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{driverStandings.length}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>RACES</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{results.length}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>WORLD CHAMPIONSHIPS</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbChampionships}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>WINS</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbWins}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>PODIUMS</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{nbPodiums}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                    <Col md={6} lg={4} className="mb-3">
-                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
-                            <p className="mb-0" style={textBlack}>TEAMS</p>
-                            <p className="text-center mb-0" style={{...textBold, fontSize: "20px"}}>{teams.length}</p>
-                            <div></div>
-                        </Container>
-                    </Col>
-                </Row>
+                <AllDriversOneDetailsContainer
+                    nationality={driver?.nationality}
+                    dateOfBirth={driver?.dateOfBirth}
+                    firstSeason={results[0]?.season}
+                    firstRace={results[0]?.raceName}
+                    nbSeasons={driverStandings.length}
+                    nbRaces={results.length}
+                    nbChampionships={nbChampionships}
+                    nbWins={nbWins}
+                    nbPodiums={nbPodiums}
+                    nbTeams={teams.length}
+                />
             </Container>
         );
     }
