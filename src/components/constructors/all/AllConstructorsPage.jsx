8b05f96d7d586737cf6121bd2a5fd6fa56dee427
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-03-04 16:48:26 +0100

    Ajout des données de tout les constructeurs

--- src/components/constructors/all/AllConstructorsPage.jsx
+++ src/components/constructors/all/AllConstructorsPage.jsx
@@ -5,6 +5,25 @@ import { flagsNationality } from "../../../constants/flagsNationality";
 import { Container, Row, Col, Spinner, Card, Image } from "react-bootstrap";
 
 export default function AllConstructorsPage(){
+    const [constructors, setConstructors] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchInfo = async () => {
+        try{
+            const response = await fetch("http://ergast.com/api/f1/constructors.json?limit=250");
+            const data = await response.json();
+            setConstructors(data.MRData.ConstructorTable.Constructors);
+        }catch(error){
+            console.log(error);
+        }finally{
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchInfo();
+    }, [])
+
     const textRegular = {
         fontFamily: "Formula1-Regular",
         letterSpacing: "0.0005rem",
@@ -14,26 +33,39 @@ export default function AllConstructorsPage(){
         letterSpacing: "0.0005rem",
     }
 
-    return (
-        <Container>
-            <h1 className="fst-italic mt-1" style={textRegular}>All Constructors</h1>
-            <Row>
-                <Col sm={6} md={4} lg={3} xl={2} className="mb-2 p-1">
-                <a href="#" className="link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
-                    <Card style={{height: "8rem"}}>
-                        <Card.Body className="d-flex flex-column justify-content-between">
-                            <Card.Title>
-                                <span style={textBold}>Example name</span>
-                            </Card.Title>
-                            <Card.Subtitle className="d-flex align-items-center">
-                                <Image src={flagsNationality["French"]} rounded className="me-1 border" style={{height: 20}} />
-                                <span className="fst-italic" style={textRegular}>French</span>
-                            </Card.Subtitle>
-                        </Card.Body>
-                    </Card>
-                </a>
-                </Col>
-            </Row>
-        </Container>
-    )
+    if(isLoading){
+        return(
+            <Spinner animation="border" style={{color: "#ff1801"}} />
+        );
+    }
+    else{
+        return (
+            <Container>
+                <h1 className="fst-italic mt-1" style={textRegular}>All Constructors</h1>
+                <Row>
+                    {
+                        constructors.map((constructor, index)=>{
+                            return (
+                                <Col sm={6} md={4} lg={3} xl={2} className="mb-2 p-1" key={index}>
+                                    <a href="#" className="link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
+                                        <Card style={{height: "8rem"}}>
+                                            <Card.Body className="d-flex flex-column justify-content-between">
+                                                <Card.Title>
+                                                    <span style={textBold}>{constructor?.name}</span>
+                                                </Card.Title>
+                                                <Card.Subtitle className="d-flex align-items-center">
+                                                    <Image src={flagsNationality[constructor?.nationality]} rounded className="me-1 border" style={{height: 20}} />
+                                                    <span className="fst-italic" style={textRegular}>{constructor?.nationality}</span>
+                                                </Card.Subtitle>
+                                            </Card.Body>
+                                        </Card>
+                                    </a>
+                                </Col>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
+        )
+    }
 }
