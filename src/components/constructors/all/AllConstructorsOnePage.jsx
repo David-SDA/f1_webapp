397e796dfd46532c44197c658c2faaafefd4b1df
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-03-05 17:48:14 +0100

    Ajout de la nationalité du constructeurs

--- src/components/constructors/all/AllConstructorsOnePage.jsx
+++ src/components/constructors/all/AllConstructorsOnePage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, Spinner } from "react-bootstrap";
+import { flagsNationality } from "../../../constants/flagsNationality";
+
+import { Container, Row, Col, Image, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 export default function AllConstructorsOnePage(){
@@ -31,6 +33,10 @@ export default function AllConstructorsOnePage(){
         fontFamily: "Formula1-Bold",
         letterSpacing: "0.0005rem",
     }
+    const textBlack = {
+        fontFamily: "Formula1-Black",
+        letterSpacing: "0.0005rem",
+    }
 
     if(isLoading){
         return(
@@ -41,6 +47,18 @@ export default function AllConstructorsOnePage(){
         return (
             <Container>
                 <h1 className="fst-italic mt-1" style={textBold}>{constructor?.name}</h1>
+                <Row className="d-flex justify-content-center">
+                    <Col md={6} lg={4}>
+                        <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                            <p className="mb-0" style={textBlack}>NATIONALITY</p>
+                            <div className="d-flex flex-row justify-content-center align-items-center">
+                                <Image src={flagsNationality[constructor?.nationality]} rounded className="me-1 border" style={{height: 25}} />
+                                <p className="mb-0" style={{...textBold, fontSize: "20px"}}>{constructor?.nationality}</p>
+                            </div>
+                            <div></div>
+                        </Container>
+                    </Col>
+                </Row>
             </Container>
         )
     }
