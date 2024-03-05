a9b76125bd57c589435abdd4a2d4029805d17872
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-03-05 18:13:04 +0100

    Séparation en composant

---
+++ src/components/constructors/all/AllConstructorsOneDetailsContainer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { flagsNationality } from "../../../constants/flagsNationality";
+
+import { Col, Container, Image, Row } from "react-bootstrap";
+
+export default function AllConstructorsOneDetailsContainer({
+    nationality
+}){
+    const textBold = {
+        fontFamily: "Formula1-Bold",
+        letterSpacing: "0.0005rem",
+    }
+
+    const textBlack = {
+        fontFamily: "Formula1-Black",
+        letterSpacing: "0.0005rem",
+    }
+
+    return (
+        <Row className="d-flex justify-content-center">
+            <Col md={6} lg={4}>
+                <Container className="d-flex flex-column justify-content-around rounded-4" style={{height: "100px", borderRight: "5px solid #ff1801", borderBottom: "5px solid #ff1801"}}>
+                    <p className="mb-0" style={textBlack}>NATIONALITY</p>
+                    <div className="d-flex flex-row justify-content-center align-items-center">
+                        <Image src={flagsNationality[nationality]} rounded className="me-1 border" style={{height: 25}} />
+                        <p className="mb-0" style={{...textBold, fontSize: "20px"}}>{nationality}</p>
+                    </div>
+                    <div></div>
+                </Container>
+            </Col>
+        </Row>
+    )
+}
