75cee41643c3564d22c5958ee102cce4e22bb824
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-03-13 15:51:31 +0100

    Ajout vérification de l'année dans url

--- src/components/seasons/OneSeasonPage.jsx
+++ src/components/seasons/OneSeasonPage.jsx
@@ -3,9 +3,26 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 import { useParams } from "react-router-dom";
+import NotFoundPage from "../NotFoundPage";
+
+const isValidSeason = (year) => {
+    const currentYear = new Date().getFullYear();
+    const minYear = 1950;
+
+    if(!isNaN(year) && year >= minYear && year <= currentYear){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 export default function OneSeasonPage(){
     const { season } = useParams();
+
+    if(!isValidSeason(season)){
+        return <NotFoundPage />;
+    }
 
     const textRegular = {
         fontFamily: "Formula1-Regular",
