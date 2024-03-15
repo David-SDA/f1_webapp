89e597ad347afdf276c904c30519380d58fd8e9f
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-03-15 11:43:11 +0100

    Séparation du header du classement des pilotes en composant

--- src/components/seasons/OneSeasonPage.jsx
+++ src/components/seasons/OneSeasonPage.jsx
@@ -5,6 +5,7 @@ import { Col, Container, Row, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import NotFoundPage from "../NotFoundPage";
 import OneSeasonOneRace from "./OneSeasonOneRace";
+import OneSeasonDriverStandingsHeader from "./OneSeasonDriverStandingsHeader";
 
 const isValidSeason = (year) => {
     const currentYear = new Date().getFullYear();
@@ -62,10 +63,6 @@ export default function OneSeasonPage(){
         fontFamily: "Formula1-Bold",
         letterSpacing: "0.0005rem",
     }
-    const textBlack = {
-        fontFamily: "Formula1-Black",
-        letterSpacing: "0.0005rem",
-    }
     const textWide = {
         fontFamily: "Formula1-Wide",
         letterSpacing: "0.0005rem",
@@ -103,42 +100,7 @@ export default function OneSeasonPage(){
                 </Container>
                 <h2 className="fst-italic mt-1" style={textRegular}>DRIVER STANDINGS</h2>
                 <Container className="mb-2 pt-3 pb-3 rounded" style={{backgroundColor: "#38383f"}}>
-                    <Row className="d-flex flex-nowrap flex-row justify-content-around align-items-center bg-white m-1 p-1 p-sm-2 rounded-3">
-                        <Col className="p-0" xs={1} sm={1} md={1} lg={1}>
-                            <p className="m-0 text-center d-none d-lg-block" style={textBlack}>
-                                POSITION
-                            </p>
-                            <p className="m-0 text-center d-block d-lg-none" style={textBlack}>
-                                POS
-                            </p>
-                        </Col>
-                        <Col className="p-0" xs={5} sm={3} md={4} lg={4}>
-                            <p className="m-0 d-none d-sm-block" style={textBlack}>
-                                DRIVER
-                            </p>
-                            <p className="m-0 d-block d-sm-none" style={textBlack}>
-                                DRI
-                            </p>
-                        </Col>
-                        <Col className="p-0 d-none d-sm-block" sm={3} md={4} lg={3}>
-                            <p className="m-0 text-center" style={textBlack}>
-                                TEAM
-                            </p>
-                        </Col>
-                        <Col className="p-0 d-none d-md-block" sm={1} md={1} lg={1}>
-                            <p className="m-0 text-center" style={textBlack}>
-                                WINS
-                            </p>
-                        </Col>
-                        <Col className="p-0" xs={3} sm={1} md={1} lg={1}>
-                            <p className="m-0 text-center d-none d-lg-block" style={textBlack}>
-                                POINTS
-                            </p>
-                            <p className="m-0 text-center d-block d-lg-none" style={textBlack}>
-                                PTS
-                            </p>
-                        </Col>
-                    </Row>
+                    <OneSeasonDriverStandingsHeader />
                     {
                         driverStandings.map((driver, index) => {
                             return (
