94ad9f332cb3bf4a664a1c29fa514ada9d1c7896
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-06-12 17:24:32 +0200

    Ajout du cache pour le top 3 pilotes

--- src/components/standings/TopThreeDriversContainer.jsx
+++ src/components/standings/TopThreeDriversContainer.jsx
@@ -11,12 +11,44 @@ export default function TopThreeDriversContainer(){
 
     const fetchInfo = async () => {
         try{
+            // Vérification si les données sont en cache
+            const cachedTopThreeDrivers = localStorage.getItem('topThreeDrivers');
+            // On récupère la date actuelle
+            const currentDateTime = new Date().getTime();
+            //console.log('Fetching top 3 drivers data...');
+
+            // Si les données sont en cache
+            if(cachedTopThreeDrivers){
+                // On extrait les données du cache
+                const { topThreeDrivers, timestamp } = JSON.parse(cachedTopThreeDrivers);
+                // On extrait la date du lendemain
+                const oneDayFromNow = timestamp + 24 * 60 * 60 * 1000;
+                //console.log('Found cached data:', topThreeDrivers);
+
+                // Si la date actuelle est avant la date du lendemain, on utilise les données du cache
+                if(currentDateTime < oneDayFromNow){
+                    //console.log('Using cached data...');
+                    setStandings(topThreeDrivers);
+                    setIsLoading(false);
+                    return;
+                }
+                else{
+                    //console.log('Cached data is outdated. Removing...');
+                    localStorage.removeItem('topThreeDrivers');
+                }
+            }
+            //console.log('Making API call...');
+            // On fait l'appel API ainsi que la sauvegarde dans le cache
             const response = await fetch('https://ergast.com/api/f1/current/driverStandings.json?limit=3');
             const data = await response.json();
-            setStandings(data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
-        }catch(error){
+            const topThreeDrivers = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+            setStandings(topThreeDrivers);
+            localStorage.setItem('topThreeDrivers', JSON.stringify({ topThreeDrivers, timestamp: new Date().getTime() }));
+        }
+        catch(error){
             console.log(error);
-        }finally{
+        }
+        finally{
             setIsLoading(false);
         }
     };
