d6218946e6ea6724d79141db804369976c7d8266
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-06-19 14:48:35 +0200

    Ajout du cache pour les résultats du sprint

--- src/components/raceResults/SprintResultsContainer.jsx
+++ src/components/raceResults/SprintResultsContainer.jsx
@@ -10,14 +10,53 @@ export default function SprintResultsContainer({round}){
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+    // Fonction pour connaitre le prochain lundi
+    const getNextMonday = () => {
+        const d = new Date();
+        d.setDate(d.getDate() + (((1 + 7 - d.getDay()) % 7) || 7));
+        d.setHours(0, 0, 0, 0);
+        return d.getTime();
+    };
+
     const fetchInfo = async () => {
         try{
+            // Vérification si les données sont en cache
+            const cachedData = localStorage.getItem('nowSprintResults_' + round);
+            // On détermine la date actuelle
+            const currentDateTime = new Date().getTime();
+            //console.log('Fetching results data...');
+
+            // Si les données sont en cache
+            if(cachedData){
+                // On extrait les données du cache
+                const { results } = JSON.parse(cachedData);
+                const nextMonday = getNextMonday();
+                //console.log('Found cached data:', results);
+
+                // Si la date actuelle est avant le prochain lundi, on utilise les données du cache
+                if(currentDateTime < nextMonday){
+                    //console.log('Using cached data...');
+                    setResults(results);
+                    setIsLoading(false);
+                    return;
+                }
+                else{
+                    //console.log('Cached data is outdated. Removing...');
+                    localStorage.removeItem('nowSprintResults_' + round);
+                }
+            }
+            //console.log('Fetching data from API...');
+            // On fait l'appel à l'API ainsi que la ssauvegarde dans le cache
             const response = await fetch("http://ergast.com/api/f1/current/" + round + "/sprint.json");
             const data = await response.json();
-            setResults(data.MRData.RaceTable.Races[0].SprintResults);
-        }catch(error){
+            const sprintResults = data.MRData.RaceTable.Races[0].SprintResults;
+            setResults(sprintResults);
+            localStorage.setItem('nowSprintResults_' + round, JSON.stringify({ results: sprintResults }));
+        }
+        catch(error){
             console.log(error);
-        }finally{
+        }
+        finally{
             setIsLoading(false);
         }
     }
