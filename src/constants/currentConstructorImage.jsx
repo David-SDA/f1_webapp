2a7d07a749d396e7153bda5fe601bdfa8b4fb64e
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-06-19 15:49:27 +0200

    Mise à jour des team logo

--- src/constants/currentConstructorImage.jsx
+++ src/constants/currentConstructorImage.jsx
@@ -1,12 +1,12 @@
 export const  currentConstructorImage = {
     "red_bull": require("../assets/teamsLogo/redbull.jpeg"),
-    "ferrari": require("../assets/teamsLogo/ferrari.jpeg"),
-    "mercedes": require("../assets/teamsLogo/mercedes.jpeg"),
-    "aston_martin": require("../assets/teamsLogo/aston-martin.jpeg"),
+    "ferrari": require("../assets/teamsLogo/ferrari.jpg"),
+    "mercedes": require("../assets/teamsLogo/mercedes.jpg"),
+    "aston_martin": require("../assets/teamsLogo/aston-martin.jpg"),
     "alpine": require("../assets/teamsLogo/alpine.jpg"),
     "mclaren": require("../assets/teamsLogo/mclaren.jpg"),
     "williams": require("../assets/teamsLogo/williams.jpg"),
     "haas": require("../assets/teamsLogo/haas.jpg"),
-    "alfa": require("../assets/teamsLogo/alfa-romeo.jpg"),
-    "alphatauri": require("../assets/teamsLogo/alphatauri.jpg"),
+    "sauber": require("../assets/teamsLogo/sauber.jpg"),
+    "rb": require("../assets/teamsLogo/rb.jpg"),
 }
