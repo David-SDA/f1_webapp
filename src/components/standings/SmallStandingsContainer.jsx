566df250a666e1669350d0a4ac36790366b1a97b
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-07-05 11:30:17 +0200

    Ajout visuel sur les boutons lors du passage de la souris

--- src/components/standings/SmallStandingsContainer.jsx
+++ src/components/standings/SmallStandingsContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TopThreeDriversContainer from "./TopThreeDriversContainer";
 import TopThreeConstructorsContainer from "./TopThreeConstructorsContainer";
@@ -6,16 +6,46 @@ import TopThreeConstructorsContainer from "./TopThreeConstructorsContainer";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
 export default function SmallStandingsContainer(){
+    const [isHoveredDriver, setIsHoveredDriver] = useState(false);
+    const [isHoveredConstructor, setIsHoveredConstructor] = useState(false);
+
+    const baseStyle = {
+        fontFamily: "Formula1-Bold",
+        backgroundColor: "#ff1801",
+        fontSize: 14,
+        border: "0",
+    };
+
+    const hoverStyle = {
+        backgroundColor: "#cc1400",
+    };
+
     return (
         <Container>
             <Row direction="horizontal" gap={1} className="d-flex justify-content-evenly mb-4">
                 <Col xs={12} lg={5} className="d-flex flex-column align-items-end rounded-3 shadow pt-3 pb-3 ps-1 ps-sm-3 pe-1 pe-sm-3 mb-1" style={{backgroundColor: "#38383f"}}>
                     <TopThreeDriversContainer />
-                    <Button href="/driverStandings" className="border-0 mt-1" style={{fontFamily: "Formula1-Bold", backgroundColor: "#ff1801", fontSize: 14}}>Full Drivers Standings &gt;</Button>
+                    <Button
+                        href="/driverStandings"
+                        className="border-0 mt-1"
+                        style={isHoveredDriver ? { ...baseStyle, ...hoverStyle } : baseStyle}
+                        onMouseEnter={() => setIsHoveredDriver(true)}
+                        onMouseLeave={() => setIsHoveredDriver(false)}
+                    >
+                        Full Drivers Standings &gt;
+                    </Button>
                 </Col>
                 <Col xs={12} lg={5} className="d-flex flex-column align-items-end rounded-3 shadow pt-3 pb-3 ps-1 ps-sm-3 pe-1 pe-sm-3 mb-1" style={{backgroundColor: "#38383f"}}>
                     <TopThreeConstructorsContainer />
-                    <Button href="/constructorStandings" className="border-0 mt-1" style={{fontFamily: "Formula1-Bold", backgroundColor: "#ff1801", fontSize: 14}}>Full Constructors Standings &gt;</Button>
+                    <Button
+                        href="/constructorStandings"
+                        className="border-0 mt-1"
+                        style={isHoveredConstructor ? { ...baseStyle, ...hoverStyle } : baseStyle}
+                        onMouseEnter={() => setIsHoveredConstructor(true)}
+                        onMouseLeave={() => setIsHoveredConstructor(false)}
+                    >
+                        Full Constructors Standings &gt;
+                    </Button>
                 </Col>
             </Row>
         </Container>
